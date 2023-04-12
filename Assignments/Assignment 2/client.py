ec7d3c9de1c6bf3a8d6994dc67849cbd57d44d92
David <dbobek29@gmail.com>
2023-04-12 20:14:48 +0200

    creating users, gertting users, authorization of the user all works

---
+++ Assignments/Assignment 2/client.py
@@ -0,0 +1,74 @@
+import requests
+
+
+from enum import Enum
+
+from validation_functions import generate_token, validate_token
+
+
+class Role(Enum):
+    Administrator = 1
+
+    Secretary = 2
+
+    Manager = 3
+
+    def __str__(self):
+        return f"{self.value}"
+
+
+class User:
+    def __init__(self, username, password) -> None:
+        self.username = username
+        self.password = password
+        self.role = None
+        self.token = None
+
+
+def admin_creation():
+    john = User(username="john", password="password")
+    john.role = Role.Administrator
+    token = generate_token("username")
+    john.token = token
+
+    return john
+
+
+# create a user with a role and a token on the server
+def create_user(user):
+    url = "http://localhost:8000/auth/user/"
+    params = {
+        "user_role": user.role,
+        "username": "john",
+        "password": "password123",
+        "new_role": "1",
+        "token": user.token,
+    }
+    response = requests.post(url, params=params)
+    if response.status_code == 200:
+        print("Creating user on server")
+        data = response.json()
+        print(data)
+
+# get a user from the server with a username
+def get_user(user, username):
+    url = "http://localhost:8000/auth/get_users"
+    params = {
+        "user_role": user.role,
+        "username": username,
+        "token": user.token,
+    }
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        print("getting users from server")
+        data = response.json()
+        print(data)
+
+
+if __name__ == "__main__":
+    user = admin_creation()
+    create_user(user)
+    get_user(user,"john")
+
+    #When I run this code I get the following response: User does not exist
+    #get_user(user,"john2")
---
+++ Assignments/Assignment 2/server.py
@@ -0,0 +1,84 @@
+# Server that handles requests from the client and sends back the response, but first uses authentication to verify the client using JWT
+
+
+from fastapi import FastAPI, Depends, HTTPException, status
+
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
+from validation_functions import generate_token, validate_token
+
+from typing import Optional
+
+
+""" john = User(username="john", password="password")
+john.role = Role.Administrator
+
+
+
+print(john.role)
+"""
+from enum import Enum
+
+users = {}
+
+
+class Role(Enum):
+    Administrator = 1
+
+    Secretary = 2
+
+    Manager = 3
+
+    def __str__(self):
+        return f"{self.value}"
+
+
+class User:
+    def __init__(self, username, password) -> None:
+        self.username = username
+        self.password = password
+        self.role = None
+
+
+app = FastAPI()
+
+
+# authentication of the user with JWT
+
+users = []
+usernames = []
+tokens = {}
+
+
+# create a user with a role and a token
+@app.post("/auth/user/")
+async def create_user(user_role, username, password, new_role, token):
+
+    tokens[username] = token
+    if validate_token(token, "username"):
+        if username not in usernames:
+            print("Token is valid \n", token)
+            user = User(username=username, password=password)
+            user.role = new_role
+            users.append(user)
+            usernames.append(username)
+            return "User created"
+        else:
+            return "User already exists"
+    else:
+        return "Token is invalid"
+
+
+# get the user role by username
+@app.get("/auth/get_users/")
+async def get_user(user_role, username,token):
+    if validate_token(token, "username"):
+        if username in usernames:
+            print("Token is valid \n")
+            #return role of the user by username
+            print("users", users)
+            return  [user.role for user in users if user.username == username]
+        else:
+            return "User does not exist"
+    else:
+        return "Token is invalid"
